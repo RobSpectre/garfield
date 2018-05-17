4448ce648f7829eebfafdd5457d5999987823e06
Rob Spectre <rob@brooklynhacker.com>
2018-05-17 12:21:24 -0400

    1) Added tests for `DeterrenceMessageInline`.

---
+++ garfield/deterrence/tests/test_admin.py
@@ -0,0 +1,23 @@
+from django.test import RequestFactory
+from django.test import TestCase
+
+from deterrence.admin import DeterrenceMessageInline
+
+
+class DeterrenceInlineTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.request = self.factory.get("/stuff")
+
+    def test_deterrence_message_inline_get_extra(self):
+        test = DeterrenceMessageInline.get_extra(None, self.request)
+
+        self.assertEquals(test, 1)
+
+    def test_deterrence_message_inline_get_extra_obj_exists(self):
+        test = DeterrenceMessageInline.get_extra(None,
+                                                 self.request,
+                                                 obj=True)
+
+        self.assertEquals(test, 0)
