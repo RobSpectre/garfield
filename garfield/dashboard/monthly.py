11548b130ec28aad8b4193be818a59dd72be62fe
Rob Spectre <rob@brooklynhacker.com>
2018-05-17 17:30:07 -0400

    1) Added deliverability information to monthly deterrence message chart.

--- garfield/dashboard/monthly.py
+++ garfield/dashboard/monthly.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django.db.models import Q
 from django.db.models.functions import TruncMonth
 
 from controlcenter import Dashboard
@@ -63,16 +64,66 @@ class MonthlyCallChart(MonthlyChart):
                 .annotate(count=Count('id')))
 
 
-class MonthlyDeterrenceMessageChart(MonthlyChart):
-    title = "Monthly Deterrence MessageVolume"
+class MonthlyDeterrenceMessageChart(widgets.BarChart):
+    class Chartist:
+        options = {"axisX": {"onlyInteger": True},
+                   "stackBars": True}
 
-    values_list = ('month_created', 'count')
+    title = "Monthly Deterrence Messages"
+
+    width = widgets.SMALL
+
+    failed = Count('id', filter=Q(status='failed'))
+    delivered = Count('id', filter=Q(status='delivered'))
+    sent = Count('id', filter=Q(status='sent'))
+    undelivered = Count('id', filter=Q(status='undelivered'))
 
     queryset = (DeterrenceMessage.objects
                 .annotate(month_created=TruncMonth('date_created'))
                 .order_by('month_created')
                 .values('month_created')
-                .annotate(count=Count('id')))
+                .annotate(delivered=delivered)
+                .annotate(sent=sent)
+                .annotate(undelivered=undelivered)
+                .annotate(failed=failed))
+
+    def legend(self):
+        return ['failed',
+                'sent',
+                'delivered',
+                'undelivered']
+
+    def labels(self):
+        labels = []
+
+        for item in self.queryset:
+            labels.append(item['month_created'])
+
+        return [label.strftime("%b") for label in labels]
+
+    def series(self):
+        values = {}
+
+        for row in self.queryset:
+            values[row['month_created'].strftime("%b")] = \
+                {'delivered': row['delivered'],
+                 'sent': row['sent'],
+                 'undelivered': row['undelivered'],
+                 'failed': row['failed']}
+
+        series = []
+
+        for key in self.legend:
+            row = []
+            for label in self.labels:
+                item = values.get(label, {'delivered': 0,
+                                          'sent': 0,
+                                          'undelivered': 0,
+                                          'failed': 0})
+                row.append(item[key])
+            series.append(row)
+
+        return series
 
 
 class MonthlyDeterrenceResponseChart(MonthlyChart):
