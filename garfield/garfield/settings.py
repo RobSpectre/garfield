26c33e6d7153c93569d1d2ad36d8ea3727ac2210
Sail338 <srihari.shankar1@gmail.com>
2018-07-15 13:35:59 -0700

    remove twilio creds from settings

--- garfield/garfield/settings.py
+++ garfield/garfield/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'CHANGE ME'
-SID= 'TWILIO SID'
-AUTH= 'TWILIO AUTH'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
