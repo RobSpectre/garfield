55b5e74391bee3b5582ca152a3a371d4d6fadf05
SaraAnn Stanway <sarastanway@gmail.com>
2018-07-16 21:37:31 -0700

    fixing syntax

--- garfield/lookup/models.py
+++ garfield/lookup/models.py
@@ -2,18 +2,17 @@ from django.db import models
 from contacts.models import Contact
 
 # Create your models here.
-class Lookup(model.Model):
+class Lookup(models.Model):
     look_up_date = models.DateTimeField(auto_now_add=True)
     officer_phone_number = models.CharField(max_length=12,
-                                            db_index=True
-                                            blank=False
+                                            db_index=True,
+                                            blank=False,
                                             null=False)
     contact_phone_number = models.CharField(max_length=15,
                                             db_index=True,
                                             blank=False,
                                             null=False)
-
-    related_contact = models.ManyToOneField(Contact, null=True)
+    related_contact = models.ForeignKey(Contact, null=True, on_delete=models.SET_NULL)
 
     def save(self,force_insert=False,force_update=False,**kwargs):
         super(Lookup,self).save(force_insert,force_update)
