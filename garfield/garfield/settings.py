c9c9520a19a350170264fc500ed06193ea9d5dfe
SaraAnn Stanway <sarastanway@gmail.com>
2018-07-24 23:03:26 -0700

    seriously? pep8?

--- garfield/garfield/settings.py
+++ garfield/garfield/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'CHANGE ME'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0ce26665.ngrok.io', 'tunnel.brooklynhacker.com']
+ALLOWED_HOSTS = ['tunnel.brooklynhacker.com']
 
 
 # Application definition
