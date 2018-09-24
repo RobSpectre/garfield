47f31ede334b68ba78101ecfa4f725e2593b57bc
Rob Spectre <rob@brooklynhacker.com>
2018-09-24 14:52:40 -0400

    1) Added management command to add contacts from supplied spreadsheet.

---
+++ garfield/contacts/management/commands/add_manual_contacts.py
@@ -0,0 +1,54 @@
+import csv
+from datetime import datetime
+
+import phonenumbers
+
+from django.core.management.base import BaseCommand
+
+from contacts.models import Contact
+
+
+class Command(BaseCommand):
+    help = "Add contacts manually from a spreadsheet."
+
+    def add_arguments(self, parser):
+        parser.add_argument("csv_file_path", type=str)
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Opening CSV file: {0}..."
+                          "".format(kwargs['csv_file_path']))
+
+        with open(kwargs['csv_file_path'], 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+
+            for row in reader:
+                self.stdout.write("Checking contact: {0}"
+                                  "".format(row['phone']))
+
+                number = phonenumbers.parse(row['phone'],
+                                            'US')
+                e164 = phonenumbers.format_number(number,
+                                                  phonenumbers
+                                                  .PhoneNumberFormat
+                                                  .E164)
+
+                check = Contact.objects.filter(phone_number=e164)
+
+                if len(check) > 0:
+                    self.stdout.write("Contact exists, skipping.")
+                else:
+                    self.stdout.write("Adding contact: {0}"
+                                      "".format(row['phone']))
+                    timestamp = "{0} 2018 18:00:00+0000".format(row['date'])
+
+                    self.stdout.write("Timestamp is: {0}"
+                                      "".format(timestamp))
+
+                    timestamp = datetime.strptime(timestamp,
+                                                  '%d-%b %Y %H:%M:%S%z')
+
+                    contact = Contact(phone_number=number)
+                    contact.save()
+                    contact.date_created = timestamp
+                    contact.save()
+                    self.stdout.write("Contact added.")
